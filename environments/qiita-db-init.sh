b05fdae3aeb9d3d1faedadeec9fa15f94db78734
Anna-Rehm <Anna.Rehm@computational.bio.uni-giessen.de>
2024-05-16 14:14:06 +0200

    Add qiita env/init files based on previous example

---
+++ environments/qiita-db-init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+set -u
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
+      ALTER DATABASE "$POSTGRES_DB_NAME" OWNER TO "$POSTGRES_USER"
+EOSQL
