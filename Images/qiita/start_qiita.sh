083777149645dcb1c450a41581a666cb4ece5f9b
Anna-Rehm <Anna.Rehm@computational.bio.uni-giessen.de>
2024-07-30 14:30:09 +0200

    Changes after consulting with Nils

--- Images/qiita/start_qiita.sh
+++ Images/qiita/start_qiita.sh
@@ -6,12 +6,19 @@ redis-server --daemonize yes --port 6379
 
 export QIITA_CONFIG_FP="/qiita/config_qiita_oidc.cfg"
 
-conda list
+# conda list
+if [ "$( psql -XtAc "SELECT 1 FROM postgres WHERE datname='qiita_test'" )" = '1' ]
+then
+    supervisord -c /supervisor_foreground.conf
+else
+    qiita-env make --no-load-ontologies
+    supervisord -c /supervisor_foreground.conf
+fi
 
 # building the database without ontologies
-qiita-env make --no-load-ontologies # || true
+# qiita-env make --no-load-ontologies # || true
 
 # starting the webserver without building the docs
 # qiita pet webserver --no-build-docs start
 
-supervisord -c /supervisor_foreground.conf
+# supervisord -c /supervisor_foreground.conf
