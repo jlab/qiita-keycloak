fd9410a3627055d162b08f498a1c74c54a7b7608
Anna-Rehm <Anna.Rehm@computational.bio.uni-giessen.de>
2024-08-08 10:14:13 +0200

    Fixed Previous Commits, Redis can now communicate with Qiita and Qiita without supervisord works as intended.

--- Images/qiita/start_qiita.sh
+++ Images/qiita/start_qiita.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-
-# first we start the redis server
-# redis-server --daemonize yes --port 7777
-# redis-server --daemonize yes --port 6379
-
+#sleep 300000
 export QIITA_CONFIG_FP="/qiita/config_qiita_oidc.cfg"
-# TODO: kick out the supervisor -> one "master" image and one "worker" image and then they do shenanigans together via replicas
-# conda list
-if [ "$( psql -XtAc "SELECT 1 FROM postgres WHERE datname='qiita_test'" )" = '1' ]
+
+if [ "$( psql -h localhost -U postgres -XtAc "SELECT 1 FROM postgres WHERE datname='qiita_test'" )" = '1' ]
 then
-    # supervisord -c /supervisor_foreground.conf
     qiita pet webserver --no-build-docs start --port 21174 --master
 else
     qiita-env make --no-load-ontologies
     qiita pet webserver --no-build-docs start --port 21174 --master
-    # supervisord -c /supervisor_foreground.conf
 fi
-
-# building the database without ontologies
-# qiita-env make --no-load-ontologies # || true
-
-# starting the webserver without building the docs
-# qiita pet webserver --no-build-docs start
-
-# supervisord -c /supervisor_foreground.conf
