c02177942352274bf5f29c78eed323ca9727e49c
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-03-04 08:30:55 +0100

    add startup script

---
+++ Images/qiita/start_plugin.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import requests
+import json
+
+PORT = 5000
+API_ENDPOINT = "run"
+
+pluginname, qiita_server_url, job_id, output_dir = sys.argv[1:]
+
+req = requests.post('http://qiita-container-anna-%s-1:%s/run' % (pluginname, PORT),
+                    json={'url': qiita_server_url,
+                          'job_id': job_id,
+                          'output_dir': output_dir})
+print(req.status_code)
+
+retvalues = json.loads(req.text)
+print("=== request STDERR ===\n%s" % retvalues['stderr'])
+print("=== request STDOUT ===\n%s" % retvalues['stdout'])
