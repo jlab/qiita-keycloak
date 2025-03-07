316618080882547389f63892ed29df9c03205def
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-03-07 17:34:29 +0100

    hide workflows to NOT filter commands for artifact processing

--- Images/qiita/start_qiita-initDB.sh
+++ Images/qiita/start_qiita-initDB.sh
@@ -8,3 +8,9 @@ ENV_NAME=qiita
 source $CONDA_DIR/etc/profile.d/conda.sh; conda activate $CONDA_DIR/envs/$ENV_NAME; cd /qiita; qiita-env make --no-load-ontologies 2> .env-make.err || true
 # To avoid confusing the user, STDERR is written into a file and only reported if it does not contain the text that we expect to see if it just reports the existing DB
 grep 'already present on the system. You can drop it by running' .env-make.err > /dev/null || cat .env-make.err
+
+
+# currently, commands with which you can process artifacts are limited to those that are present in available "recommended workflows".
+# As they are not properly set up in the test database, we e.g. cannot run "deblur" on demux or trimmed existing artifacts (it's different in workflows in construction)
+# As long as we don't have a nice mechanism to carry over recommended workflow, we better remove them altogether
+source $CONDA_DIR/etc/profile.d/conda.sh; conda activate $CONDA_DIR/envs/$ENV_NAME; python /drop_workflows.py
