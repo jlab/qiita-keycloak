36f3e7818ca5fd8dbc126237d10067b8871835a7
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-05-08 15:33:08 +0200

    ensure workdir for qiita exists

--- Images/qiita/start_qiita-initDB.sh
+++ Images/qiita/start_qiita-initDB.sh
@@ -4,6 +4,9 @@ CONDA_DIR=/opt/conda
 ENV_NAME=qiita
 #PORT=21174
 
+# create (empty) workding directory as given in Qiita configuration
+mkdir -p `grep ^WORKING_DIR $QIITA_CONFIG_FP | cut -d "=" -f 2`
+
 # We execute qiita-env make every time. We expect that it will fail always but the very first time, as the qiita DB should exist from then on
 source $CONDA_DIR/etc/profile.d/conda.sh; conda activate $CONDA_DIR/envs/$ENV_NAME; cd /qiita; qiita-env make --no-load-ontologies 2> .env-make.err || true
 # To avoid confusing the user, STDERR is written into a file and only reported if it does not contain the text that we expect to see if it just reports the existing DB
