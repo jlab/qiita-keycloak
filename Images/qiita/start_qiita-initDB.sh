b6382f7a95c6e54d32fc47cb40fd97a8f7a6299e
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-05-09 15:56:20 +0200

    give postgres some time to initialize before speaking against the DB

--- Images/qiita/start_qiita-initDB.sh
+++ Images/qiita/start_qiita-initDB.sh
@@ -7,6 +7,8 @@ ENV_NAME=qiita
 # create (empty) workding directory as given in Qiita configuration
 mkdir -p `grep ^WORKING_DIR $QIITA_CONFIG_FP | cut -d "=" -f 2`
 
+sleep 5 # wait for postgress to boot up
+
 # We execute qiita-env make every time. We expect that it will fail always but the very first time, as the qiita DB should exist from then on
 source $CONDA_DIR/etc/profile.d/conda.sh; conda activate $CONDA_DIR/envs/$ENV_NAME; cd /qiita; qiita-env make --no-load-ontologies 2> .env-make.err || true
 # To avoid confusing the user, STDERR is written into a file and only reported if it does not contain the text that we expect to see if it just reports the existing DB
