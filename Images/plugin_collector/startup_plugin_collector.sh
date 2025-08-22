aa084796b78c33908ed157580859bb34ac20c39b
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-08-22 09:46:31 +0200

    in the future, certificate files shall be mounted directly into containers to avoid having differing content and then SSL errors

--- Images/plugin_collector/startup_plugin_collector.sh
+++ Images/plugin_collector/startup_plugin_collector.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# copy certificates into shared volume, if not present there already
-for f in `find /unshared_certificates -type f -name "*" | xargs`; do
-	test -f /qiita_certificates/`basename $f` || cp -v $f /qiita_certificates/`basename $f`;
-done
-
 # it seems to be necessary to give the plugin container some lead time
 # TODO: this might be more appropriately be addressed with healthchecks in the compose file
 sleep 3
