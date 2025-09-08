7abee11019a2ce2909770912c525b7f9d7c0b06b
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-09-08 11:42:58 +0200

    only communicate with Master

--- Images/qtp-sequencing/test_qtp-sequencing.sh
+++ Images/qtp-sequencing/test_qtp-sequencing.sh
@@ -10,18 +10,22 @@ pip install pytest
 # clone plugin repository
 git clone https://github.com/qiita-spots/${PLUGIN}
 
+# NOTE: client api reset only works when communicating with Qitta Master,
+# thus, you need to directly address the port of the master container. Don't
+# go through nginx!
+
 # fix qiita base url in client
 for f in `find /usr/local/lib/python*/site-packages/qiita_client/ -name "testing.py"`; do
-    sed -i 's|URL = "https://localhost:8383"|URL = "https://tinqiita-nginx-1:8383"|' $f;
+    sed -i 's|URL = "https://localhost:8383"|URL = "https://tinqiita-qiita-1:21174"|' $f;
 done
 
 # fix qiita base url in plugin tests
 for f in `find /${PLUGIN}/*/tests/ -name 'test_*.py'`; do
-    sed -i 's|https://localhost:21174|https://tinqiita-nginx-1:8383|' $f;
+    sed -i 's|https://localhost:21174|https://tinqiita-qiita-1:21174|' $f;
 done
 
 # better save than sorry
-export QIITA_PORT=8383
+export QIITA_PORT=21174
 export QIITA_ROOTCA_CERT=$SSL_CERT_FILE
 
 # change into plugin source directory and execute actual tests
