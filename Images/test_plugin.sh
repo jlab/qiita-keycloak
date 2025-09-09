93f71510be4bdc2a8799e7e8d5284fd793403985
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-09-09 18:18:25 +0200

    escape " and '

--- Images/test_plugin.sh
+++ Images/test_plugin.sh
@@ -28,7 +28,7 @@ done
 for f in `find /${PLUGIN}/*/tests/ -name 'test_*.py'`; do
     sed -i "s|plugin(.https://localhost:8383., .register., .ignored.)|plugin('https://tinqiita-nginx-1:8383', 'register', 'ignored')|" $f;
     # below seen in qtp-biom
-    sed -i "s|plugin("https://localhost:8383", job_id, self.out_dir)|plugin("https://tinqiita-nginx-1:8383", job_id, self.out_dir)|" $f;
+    sed -i 's|plugin("https://localhost:8383", job_id, self.out_dir)|plugin("https://tinqiita-nginx-1:8383", job_id, self.out_dir)|' $f;
 done
 
 # better save than sorry
