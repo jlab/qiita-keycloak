0cbd2de1ac873ae9e9abe2a1d4934fa92f6fe579
Anna-Rehm <Anna.Rehm@computational.bio.uni-giessen.de>
2025-09-17 11:06:26 +0200

    add softlink

--- Images/qp-deblur/run_qp-deblur.sh
+++ Images/qp-deblur/run_qp-deblur.sh
@@ -5,6 +5,8 @@ cat /qiita_certificates/k8s_rootca.crt >> `python -c "import certifi; print(cert
 export REQUESTS_CA_BUNDLE=`python -c "import certifi; print(certifi.where())"`
 export SSL_CERT_FILE=`python -c "import certifi; print(certifi.where())"`
 
+ln -s /qiita_data/reference/qp-deblur/references/* /opt/conda/envs/deblur/share/fragment-insertion/ref/
+
 QIITA_SERVER_URL=$1
 JOB_ID=$2
 OUTPUT_DIR=$3
