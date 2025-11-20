2fcbb21613144ee9072331b556a8b8fc230ce7f1
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-11-20 17:13:20 +0100

    propper listening to SIGTERM for nginx

--- Images/nginx/start_nginx.sh
+++ Images/nginx/start_nginx.sh
@@ -2,4 +2,19 @@
 mkdir -p /var/run/nginx/ /usr/local/var/tmp/nginx/
 
 nginx -t -c /qiita_configuration/nginx_qiita.conf
-nginx -c /qiita_configuration/nginx_qiita.conf
+
+# proper listening and reacting to SIGTERM
+cleanup() {
+    echo "Received SIGTERM, stopping..."
+    kill "$PY_PID" 2>/dev/null
+    wait "$PY_PID"
+    exit 0
+}
+trap cleanup SIGTERM SIGINT
+
+# start nginx and safe PID in variable
+nginx -c /qiita_configuration/nginx_qiita.conf &
+PY_PID=$!
+
+# wait till nginx process is terminated
+wait "$PY_PID"
