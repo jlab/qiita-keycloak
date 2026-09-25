b3d58de48762573f251aa04c45b726bd48716e26
Stefan Janssen <stefan.m.janssen@gmail.com>
2026-09-25 22:32:53 +0200

    make insertion conditional

--- Images/qtp-genome/patch.sql
+++ Images/qtp-genome/patch.sql
@@ -1,5 +1,8 @@
 -- Sep 25, 2026
 -- Introduce two new file types for qtp-genome
 
-INSERT INTO qiita.filepath_type (filepath_type) VALUES ('assembly');
-INSERT INTO qiita.filepath_type (filepath_type) VALUES ('annotation');
+INSERT INTO qiita.filepath_type (filepath_type) VALUES ('assembly')
+ON CONFLICT (filepath_type) DO NOTHING;
+
+INSERT INTO qiita.filepath_type (filepath_type) VALUES ('annotation')
+ON CONFLICT (filepath_type) DO NOTHING;
