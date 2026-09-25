891c4f5325965a1aa6dc808c7ed541a438a28d08
Stefan Janssen <stefan.m.janssen@gmail.com>
2026-09-25 16:10:00 +0200

    adding a general mechanism to apply patches to the database creation

---
+++ Images/qtp-genome/patch.sql
@@ -0,0 +1,5 @@
+-- Sep 25, 2026
+-- Introduce two new file types for qtp-genome
+
+INSERT INTO qiita.filepath_type (filepath_type) VALUES ('assembly');
+INSERT INTO qiita.filepath_type (filepath_type) VALUES ('annotation');
